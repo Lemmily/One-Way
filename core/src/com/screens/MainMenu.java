3be7742336cd762b16a59685bb94d65b3889fed9
Emily <Lemmily0@gmail.com>
2015-05-17 17:56:30 +0100

    application basics with splash screen created

---
+++ core/src/com/screens/MainMenu.java
@@ -0,0 +1,7 @@
+package com.screens;
+
+/**
+ * Created by lemmily on 17/05/2015.
+ */
+public class MainMenu {
+}
---
+++ core/src/com/utils/Assets.java
@@ -0,0 +1,7 @@
+package com.utils;
+
+/**
+ * Created by lemmily on 17/05/2015.
+ */
+public class Assets {
+}
